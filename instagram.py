7eeeb3440371dfd60a03b17ffe5c2911d2048f49
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-08-28 17:13:17 +0500

    commit

--- instagram.py
+++ instagram.py
@@ -15,11 +15,12 @@ class Instagram:
         
     def download_photo(self):
         url = self.url
-        video = url.replace("www.", "d.dd")
-        res = requests.get(video)
-        with open("image.png", 'wb') as video:
-            video.write(res.content)
+        # image = url.replace("www.", "d.dd")
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        # with open("image.png", 'wb') as video:
+        #     video.write(res.content)
+        return soup.find_all("img", attrs={'class': 'x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3'})
         
-        return requests.get("https://www.ddinstagram.com/p/C_MwyEANzlZ/?utm_source=ig_web_copy_link").text
         
-print(Instagram("https://www.instagram.com/p/C_MwyEANzlZ/?utm_source=ig_web_copy_link").download_photo())
+print(Instagram("https://www.instagram.com/p/C_MwyEANzlZ/?utm_source=ig_web_copy_link&img_index=1").download_photo())

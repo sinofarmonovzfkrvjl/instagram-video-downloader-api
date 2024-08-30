826c43f5d3fa396f346435940aede88a248305c6
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-08-30 14:23:24 +0500

    update

--- app.py
+++ app.py
@@ -18,18 +18,8 @@ async def download(url):
         os.remove("image.png")
     except:
         pass
-    if url.startswith("https://www.instagram.com/reel/") and url.endswith("https://www.instagram.com/stories/"):
-        if Instagram(url).download_video():
-            def itervideo():
-                # os.path.join("video.mp4")
-                with open("video.mp4", "rb") as video:
-                    yield from video
-
-            return StreamingResponse(itervideo(), media_type="video/mp4")
-        else:
-            return "ERROR"
             
-    elif url.startswith("https://www.instagram.com/p/"):
+    if url.startswith("https://www.instagram.com/p/"):
         if Instagram(url).download_photo():
             def iterimage():
                 # os.path.join("image.png")
@@ -41,4 +31,12 @@ async def download(url):
         else:
             return "ERROR"
     else:
-        return "This is not instagram video's url"
+        if Instagram(url).download_video():
+            def itervideo():
+                # os.path.join("video.mp4")
+                with open("video.mp4", "rb") as video:
+                    yield from video
+
+            return StreamingResponse(itervideo(), media_type="video/mp4")
+        else:
+            return "ERROR"
